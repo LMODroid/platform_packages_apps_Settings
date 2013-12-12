1f832500e18218992b651ccad8b7e20115bc5078
Michael Bestas <mikeioannina@gmail.com>
2013-12-12 05:40:54 +0200

    Settings: Forward port lock pattern grid size (2/2)
    Co-authored-by: Alberto97 <albertop2197@gmail.com>
    Co-authored-by: d34d <clark@cyngn.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Roman Birg <roman@cyngn.com>
    Change-Id: I7078d703c218cd096d9b77c003a94b52fbce6322

--- src/com/android/settings/password/ChooseLockPassword.java
+++ src/com/android/settings/password/ChooseLockPassword.java
@@ -1098,7 +1098,8 @@ public class ChooseLockPassword extends SettingsActivity {
                     mUserId);
 
             mSaveAndFinishWorker.start(mLockPatternUtils,
-                    mChosenPassword, mCurrentCredential, mUserId);
+                    mChosenPassword, mCurrentCredential, mUserId,
+                    mLockPatternUtils.getLockPatternSize(mUserId));
         }
 
         @Override
