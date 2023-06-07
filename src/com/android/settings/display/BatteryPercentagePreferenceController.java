301b6ddc1a68318869b6f76417a442f107353b78
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-06-07 12:15:12 +0330

    Settings: BatteryPercentagePreferenceController: Use STATUS_BAR_SHOW_BATTERY_PERCENT to enable battery percent
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Ic0c78f22f3fcb03c86a3077234fab266722002bc

--- src/com/android/settings/display/BatteryPercentagePreferenceController.java
+++ src/com/android/settings/display/BatteryPercentagePreferenceController.java
@@ -15,7 +15,7 @@
  */
 package com.android.settings.display;
 
-import static android.provider.Settings.System.SHOW_BATTERY_PERCENT;
+import static com.libremobileos.providers.LMOSettings.System.STATUS_BAR_SHOW_BATTERY_PERCENT;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
@@ -67,7 +67,7 @@ public class BatteryPercentagePreferenceController extends BasePreferenceControl
     @Override
     public void updateState(Preference preference) {
         int setting = Settings.System.getInt(mContext.getContentResolver(),
-                SHOW_BATTERY_PERCENT,
+                STATUS_BAR_SHOW_BATTERY_PERCENT,
                 mContext.getResources().getBoolean(
                 R.bool.config_defaultBatteryPercentageSetting) ? 1 : 0);
 
@@ -77,7 +77,7 @@ public class BatteryPercentagePreferenceController extends BasePreferenceControl
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         boolean showPercentage = (Boolean) newValue;
-        Settings.System.putInt(mContext.getContentResolver(), SHOW_BATTERY_PERCENT,
+        Settings.System.putInt(mContext.getContentResolver(), STATUS_BAR_SHOW_BATTERY_PERCENT,
                 showPercentage ? 1 : 0);
         FeatureFactory.getFeatureFactory().getMetricsFeatureProvider()
                 .action(mContext, SettingsEnums.OPEN_BATTERY_PERCENTAGE, showPercentage);
