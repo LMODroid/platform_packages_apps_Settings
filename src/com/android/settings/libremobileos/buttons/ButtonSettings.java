73d13d5ac419306244ada09b91bc1e89f79a4e7e
Michael Bestas <mkbestas@gmail.com>
2023-07-30 16:59:50 +0000

    Settings: ButtonSettings: Update for 13 QPR3
    Change-Id: I34d0030e41971bbbc4888dcbd98d16a4d9d2b78a
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/settings/libremobileos/buttons/ButtonSettings.java
+++ src/com/android/settings/libremobileos/buttons/ButtonSettings.java
@@ -54,7 +54,7 @@ import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
-import static com.android.systemui.shared.recents.utilities.Utilities.isTablet;
+import static com.android.systemui.shared.recents.utilities.Utilities.isLargeScreen;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -500,7 +500,7 @@ public class ButtonSettings extends SettingsPreferenceFragment
                 mEnableTaskbar.setOnPreferenceChangeListener(this);
                 mEnableTaskbar.setChecked(Settings.System.getInt(getContentResolver(),
                         Settings.System.ENABLE_TASKBAR,
-                        isTablet(getContext()) ? 1 : 0) == 1);
+                        isLargeScreen(getContext()) ? 1 : 0) == 1);
                 toggleTaskBarDependencies(mEnableTaskbar.isChecked());
             }
         }
