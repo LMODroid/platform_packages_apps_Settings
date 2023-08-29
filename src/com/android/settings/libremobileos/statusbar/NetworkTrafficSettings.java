d25ce2df05e77a099c955fe2f154e717b01dfa37
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-08-29 08:34:25 +0330

    Revert "Network traffic mode for status bar [2/2]"
    This reverts commit e64ad694424a79411c9554eb48109acd4cdf5125.
    
    Change-Id: I68c812e758cf5fc929c02352c3adba6e1ec52962
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/settings/libremobileos/statusbar/NetworkTrafficSettings.java
+++ src/com/android/settings/libremobileos/statusbar/NetworkTrafficSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2021 crDroid Android Project
+ * Copyright (C) 2017-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,101 +17,96 @@
 package com.android.settings.libremobileos.statusbar;
 
 import android.content.ContentResolver;
-import android.content.Context;
 import android.os.Bundle;
-import android.os.UserHandle;
 import android.provider.Settings;
+import android.widget.Toast;
 
-import androidx.preference.ListPreference;
+import androidx.preference.DropDownPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-import com.libremobileos.support.preferences.CustomSecureSeekBarPreference;
+import com.libremobileos.support.preferences.SecureSettingSwitchPreference;
 
 public class NetworkTrafficSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener  {
 
     private static final String TAG = "NetworkTrafficSettings";
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
 
-    private CustomSecureSeekBarPreference mNetTrafficAutohideThreshold;
-    private CustomSecureSeekBarPreference mNetTrafficRefreshInterval;
-    private ListPreference mNetTrafficLocation;
-    private ListPreference mNetTrafficMode;
-    private ListPreference mNetTrafficUnits;
-    private SwitchPreference mNetTrafficAutohide;
-    private SwitchPreference mNetTrafficHideArrow;
+    private DropDownPreference mNetTrafficMode;
+    private SecureSettingSwitchPreference mNetTrafficAutohide;
+    private DropDownPreference mNetTrafficUnits;
+    private SecureSettingSwitchPreference mNetTrafficShowUnits;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.network_traffic_settings);
+        getActivity().setTitle(R.string.network_traffic_settings_title);
+
         final ContentResolver resolver = getActivity().getContentResolver();
 
-        mNetTrafficAutohideThreshold = (CustomSecureSeekBarPreference)
-                findPreference(Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
-        mNetTrafficRefreshInterval = (CustomSecureSeekBarPreference)
-                findPreference(Settings.Secure.NETWORK_TRAFFIC_REFRESH_INTERVAL);
-        mNetTrafficLocation = (ListPreference)
-                findPreference(Settings.Secure.NETWORK_TRAFFIC_LOCATION);
-        mNetTrafficLocation.setOnPreferenceChangeListener(this);
-        mNetTrafficMode = (ListPreference)
-                findPreference(Settings.Secure.NETWORK_TRAFFIC_MODE);
-        mNetTrafficAutohide = (SwitchPreference)
-                findPreference(Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
-        mNetTrafficUnits = (ListPreference)
-                findPreference(Settings.Secure.NETWORK_TRAFFIC_UNITS);
-        mNetTrafficHideArrow = (SwitchPreference)
-                findPreference(Settings.Secure.NETWORK_TRAFFIC_HIDEARROW);
+        mNetTrafficMode = findPreference(Settings.Secure.NETWORK_TRAFFIC_MODE);
+        mNetTrafficMode.setOnPreferenceChangeListener(this);
+        int mode = Settings.Secure.getInt(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_MODE, 0);
+        mNetTrafficMode.setValue(String.valueOf(mode));
 
-        int location = Settings.Secure.getIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_LOCATION, 0, UserHandle.USER_CURRENT);
-        updateEnabledStates(location);
+        mNetTrafficAutohide = findPreference(Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
+        mNetTrafficAutohide.setOnPreferenceChangeListener(this);
+
+        mNetTrafficUnits = findPreference(Settings.Secure.NETWORK_TRAFFIC_UNITS);
+        mNetTrafficUnits.setOnPreferenceChangeListener(this);
+        int units = Settings.Secure.getInt(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_UNITS, /* Mbps */ 1);
+        mNetTrafficUnits.setValue(String.valueOf(units));
+
+        mNetTrafficShowUnits = findPreference(Settings.Secure.NETWORK_TRAFFIC_SHOW_UNITS);
+        mNetTrafficShowUnits.setOnPreferenceChangeListener(this);
+
+        updateEnabledStates(mode);
+        updateForClockConflicts();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mNetTrafficLocation) {
-            int location = Integer.valueOf((String) newValue);
-            updateEnabledStates(location);
-            return true;
+        if (preference == mNetTrafficMode) {
+            int mode = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_MODE, mode);
+            updateEnabledStates(mode);
+            updateForClockConflicts();
+        } else if (preference == mNetTrafficUnits) {
+            int units = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_UNITS, units);
         }
-        return false;
+        return true;
     }
 
-    private void updateEnabledStates(int location) {
-        final boolean enabled = location != 0;
-        mNetTrafficMode.setEnabled(enabled);
+    private void updateEnabledStates(int mode) {
+        final boolean enabled = mode != 0;
         mNetTrafficAutohide.setEnabled(enabled);
-        mNetTrafficAutohideThreshold.setEnabled(enabled);
-        mNetTrafficHideArrow.setEnabled(enabled);
-        mNetTrafficRefreshInterval.setEnabled(enabled);
         mNetTrafficUnits.setEnabled(enabled);
+        mNetTrafficShowUnits.setEnabled(enabled);
     }
 
-    public static void reset(Context mContext) {
-        ContentResolver resolver = mContext.getContentResolver();
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_LOCATION, 0, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_MODE, 0, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE, 1, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 0, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_UNITS, 1, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_SHOW_UNITS, 1, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_REFRESH_INTERVAL, 2, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.NETWORK_TRAFFIC_HIDEARROW, 0, UserHandle.USER_CURRENT);
+    private void updateForClockConflicts() {
+        int clockPosition = Settings.System.getInt(getActivity().getContentResolver(),
+                STATUS_BAR_CLOCK_STYLE, 2);
+
+        if (clockPosition != 1) {
+            return;
+        }
+
+        mNetTrafficMode.setEnabled(false);
+        Toast.makeText(getActivity(),
+                R.string.network_traffic_disabled_clock,
+                Toast.LENGTH_LONG).show();
+        updateEnabledStates(0);
     }
 
     @Override
--- src/com/android/settings/libremobileos/statusbar/StatusBarSettings.java
+++ src/com/android/settings/libremobileos/statusbar/StatusBarSettings.java
@@ -71,6 +71,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     private PreferenceCategory mStatusBarBatteryCategory;
     private PreferenceCategory mStatusBarClockCategory;
+    private Preference mNetworkTrafficPref;
 
     private boolean mHasNotch;
 
@@ -79,7 +80,12 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.status_bar_settings);
 
+        mNetworkTrafficPref = findPreference(NETWORK_TRAFFIC_SETTINGS);
+
         mHasNotch = DeviceUtils.hasNotch(getActivity());
+        if (mHasNotch) {
+            getPreferenceScreen().removePreference(mNetworkTrafficPref);
+        }
 
         mStatusBarAmPm = findPreference(STATUS_BAR_AM_PM);
         mStatusBarClock = findPreference(STATUS_BAR_CLOCK_STYLE);
@@ -125,7 +131,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
         }
 
-        final boolean disallowCenteredClock = mHasNotch;
+        final boolean disallowCenteredClock = mHasNotch || getNetworkTrafficStatus() != 0;
 
         // Adjust status bar preferences for RTL
         if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
@@ -147,6 +153,9 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             }
             mQuickPulldown.setEntries(R.array.status_bar_quick_qs_pulldown_entries);
         }
+
+        // Disable network traffic preferences if clock is centered in the status bar
+        updateNetworkTrafficStatus(getClockPosition());
     }
 
     @Override
@@ -156,6 +165,9 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         switch (key) {
             case STATUS_BAR_QUICK_QS_PULLDOWN:
                 updateQuickPulldownSummary(value);
+                break;
+            case STATUS_BAR_CLOCK_STYLE:
+                updateNetworkTrafficStatus(value);
                 break;
             case STATUS_BAR_BATTERY_STYLE:
                 enableStatusBarBatteryDependents(value);
@@ -188,6 +200,25 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mQuickPulldown.setSummary(summary);
     }
 
+    private void updateNetworkTrafficStatus(int clockPosition) {
+        if (mHasNotch) {
+            // Unconditional no network traffic for you
+            return;
+        }
+
+        boolean isClockCentered = clockPosition == 1;
+        mNetworkTrafficPref.setEnabled(!isClockCentered);
+        mNetworkTrafficPref.setSummary(getResources().getString(isClockCentered ?
+                R.string.network_traffic_disabled_clock :
+                R.string.network_traffic_settings_summary
+        ));
+    }
+
+    private int getNetworkTrafficStatus() {
+        return Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.NETWORK_TRAFFIC_MODE, 0);
+    }
+
     private int getClockPosition() {
         return Settings.System.getInt(getActivity().getContentResolver(),
                 STATUS_BAR_CLOCK_STYLE, 2);
@@ -205,7 +236,9 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         public List<String> getNonIndexableKeys(Context context) {
             final List<String> result = super.getNonIndexableKeys(context);
 
-            result.add(NETWORK_TRAFFIC_SETTINGS);
+            if (DeviceUtils.hasNotch(context)) {
+                result.add(NETWORK_TRAFFIC_SETTINGS);
+            }
             return result;
         }
     };
