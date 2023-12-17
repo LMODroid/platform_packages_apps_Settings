fde88a33f27df40e70ffbefb3fbc97590f5eef8f
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-12-18 01:14:05 +0330

    Settings: Rework on navigation bar modes and taskbar
    * Better ux with taskbar
    - Switch to full gesture mode on enabling taskbar if user is coming from other navigation bar mode
    - Disable taskbar if user enabled taskbar with full gesture mode and trying to switch to other navigation bar mode
    
    Change-Id: I65a678a87130d59e2aa628c64dcaea5542ef3023
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/settings/libremobileos/buttons/ButtonSettings.java
+++ src/com/android/settings/libremobileos/buttons/ButtonSettings.java
@@ -7,6 +7,7 @@
 package com.android.settings.libremobileos.buttons;
 
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_2BUTTON;
+import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON_OVERLAY;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL_OVERLAY;
 
@@ -656,8 +657,8 @@ public class ButtonSettings extends SettingsPreferenceFragment
             return true;
         } else if (preference == mEnableTaskbar) {
             toggleTaskBarDependencies((Boolean) newValue);
-            if ((Boolean) newValue && is2ButtonNavigationEnabled(requireContext())) {
-                // Let's switch to gestural mode if user previously had 2 buttons enabled.
+            if (((Boolean) newValue && (is2ButtonNavigationEnabled(requireContext()))) || ((Boolean) newValue && (!isLargeScreen(getContext()) && is3ButtonNavigationEnabled(requireContext())))) {
+                // Let's switch to gestural mode if user previously had 2 buttons or 3 buttons in mobile enabled.
                 setButtonNavigationMode(NAV_BAR_MODE_GESTURAL_OVERLAY);
             }
             Settings.System.putInt(getContentResolver(),
@@ -669,6 +670,11 @@ public class ButtonSettings extends SettingsPreferenceFragment
 
     private static boolean is2ButtonNavigationEnabled(Context context) {
         return NAV_BAR_MODE_2BUTTON == context.getResources().getInteger(
+                com.android.internal.R.integer.config_navBarInteractionMode);
+    }
+
+    private static boolean is3ButtonNavigationEnabled(Context context) {
+        return NAV_BAR_MODE_3BUTTON == context.getResources().getInteger(
                 com.android.internal.R.integer.config_navBarInteractionMode);
     }
 
