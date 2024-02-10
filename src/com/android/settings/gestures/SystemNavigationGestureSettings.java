BananaDroid-Lab/packages_apps_Settings

Settings: Rework on navigation bar modes and taskbar [1/2]
* Better ux with taskbar
- Switch to full gesture mode on enabling taskbar if user is coming from other navigation bar mode
- Disable taskbar if user enabled taskbar with full gesture mode and trying to switch to other navigation bar mode

Change-Id: I65a678a87130d59e2aa628c64dcaea5542ef3023
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -269,7 +269,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         }
     }
 
-    static void migrateOverlaySensitivityToSettings(Context context,
+    void migrateOverlaySensitivityToSettings(Context context,
             IOverlayManager overlayManager) {
         if (!SystemNavigationPreferenceController.isGestureNavigationEnabled(context)) {
             return;
@@ -302,17 +302,21 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
     }
 
     @VisibleForTesting
-    static void setCurrentSystemNavigationMode(IOverlayManager overlayManager, String key) {
+    void setCurrentSystemNavigationMode(IOverlayManager overlayManager, String key) {
         String overlayPackage = NAV_BAR_MODE_GESTURAL_OVERLAY;
         switch (key) {
             case KEY_SYSTEM_NAV_GESTURAL:
                 overlayPackage = NAV_BAR_MODE_GESTURAL_OVERLAY;
                 break;
             case KEY_SYSTEM_NAV_2BUTTONS:
+                disableTaskbar();
                 overlayPackage = NAV_BAR_MODE_2BUTTON_OVERLAY;
                 break;
             case KEY_SYSTEM_NAV_3BUTTONS:
                 overlayPackage = NAV_BAR_MODE_3BUTTON_OVERLAY;
+                    if (shouldDisableTaskbar()) {
+                        disableTaskbar();
+                    }
                 break;
         }
 
@@ -371,6 +375,15 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         return Settings.Secure.getInt(getContext().getContentResolver(),
                 Settings.Secure.ACCESSIBILITY_BUTTON_MODE, /* def= */ -1)
                 == ACCESSIBILITY_BUTTON_MODE_FLOATING_MENU;
+    }
+
+    private boolean disableTaskbar() {
+        return Settings.System.putInt(getContext().getContentResolver(),
+                Settings.System.ENABLE_TASKBAR, 0);
+    }
+
+    private boolean shouldDisableTaskbar() {
+        return !isLargeScreen(getContext());
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =

LMODroid/platform_packages_apps_Settings

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -271,7 +271,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         }
     }
 
-    static void migrateOverlaySensitivityToSettings(Context context,
+    void migrateOverlaySensitivityToSettings(Context context,
             IOverlayManager overlayManager) {
         if (!SystemNavigationPreferenceController.isGestureNavigationEnabled(context)) {
             return;
@@ -304,17 +304,21 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
     }
 
     @VisibleForTesting
-    static void setCurrentSystemNavigationMode(IOverlayManager overlayManager, String key) {
+    void setCurrentSystemNavigationMode(IOverlayManager overlayManager, String key) {
         String overlayPackage = NAV_BAR_MODE_GESTURAL_OVERLAY;
         switch (key) {
             case KEY_SYSTEM_NAV_GESTURAL:
                 overlayPackage = NAV_BAR_MODE_GESTURAL_OVERLAY;
                 break;
             case KEY_SYSTEM_NAV_2BUTTONS:
+                disableTaskbar();
                 overlayPackage = NAV_BAR_MODE_2BUTTON_OVERLAY;
                 break;
             case KEY_SYSTEM_NAV_3BUTTONS:
                 overlayPackage = NAV_BAR_MODE_3BUTTON_OVERLAY;
+                if (shouldDisableTaskbar()) {
+                    disableTaskbar();
+                }
                 break;
         }
 
@@ -373,6 +377,15 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         return Settings.Secure.getInt(getContext().getContentResolver(),
                 Settings.Secure.ACCESSIBILITY_BUTTON_MODE, /* def= */ -1)
                 == ACCESSIBILITY_BUTTON_MODE_FLOATING_MENU;
+    }
+
+    private boolean disableTaskbar() {
+        return Settings.System.putInt(getContext().getContentResolver(),
+                LMOSettings.System.ENABLE_TASKBAR, 0);
+    }
+
+    private boolean shouldDisableTaskbar() {
+        return !isLargeScreen(getContext());
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =