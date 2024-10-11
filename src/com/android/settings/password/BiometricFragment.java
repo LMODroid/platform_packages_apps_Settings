1baa25549b0bf1108312ba023acf24370ba43f77
Hao Dong <spdonghao@google.com>
2024-10-11 03:56:57 +0000

    Set class name when createConfirmDeviceCredentialIntent() API is used.
    Flag: EXEMPT bugfix
    Bug: 339532378
    Test: manual test
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:1ce2fd4cef984ee7d2392d9abeca7560e17f8917)
    Merged-In: I762f280def79d428ed9cf119ace5ccc2b6be2560
    Change-Id: I762f280def79d428ed9cf119ace5ccc2b6be2560

--- src/com/android/settings/password/BiometricFragment.java
+++ src/com/android/settings/password/BiometricFragment.java
@@ -143,6 +143,7 @@ public class BiometricFragment extends InstrumentedFragment {
                         promptInfo.isDisallowBiometricsIfPolicyExists())
                 .setReceiveSystemEvents(true)
                 .setAllowBackgroundAuthentication(true)
+                .setClassNameIfItIsConfirmDeviceCredentialActivity()
                 .build();
     }
 
