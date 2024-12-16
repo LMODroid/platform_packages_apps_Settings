36f599815791216e04662bdaa4c100b8a0f61b2b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-12-16 18:21:52 +0530

    Settings: Retain udfps overlay shown state on restoring activity
    Prevent udfps view from disappearing if the activity is recreated due
    to theme or configuration change while enrolling fingerprint.
    
    Change-Id: I0bb0a3cf90c8119bab566fdb0f35db0ee858b75e

--- src/com/android/settings/biometrics/fingerprint/FingerprintEnrollEnrolling.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintEnrollEnrolling.java
@@ -102,6 +102,7 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
     static final String ICON_TOUCH_DIALOG = "fps_icon_touch_dialog";
     static final String KEY_STATE_CANCELED = "is_canceled";
     static final String KEY_STATE_PREVIOUS_ROTATION = "previous_rotation";
+    static final String KEY_STATE_OVERLAY_SHOWN = "overlay_shown";
 
     private static final int PROGRESS_BAR_MAX = 10000;
 
@@ -191,6 +192,7 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
     private boolean mHaveShownSfpsLeftEdgeLottie;
     private boolean mHaveShownSfpsRightEdgeLottie;
     private boolean mShouldShowLottie;
+    private boolean mOverlayShown;
 
     private Animator mHelpAnimation;
 
@@ -387,12 +389,14 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean(KEY_STATE_CANCELED, mIsCanceled);
+        outState.putBoolean(KEY_STATE_OVERLAY_SHOWN, mOverlayShown);
         outState.putInt(KEY_STATE_PREVIOUS_ROTATION, mPreviousRotation);
     }
 
     private void restoreSavedState(Bundle savedInstanceState) {
         mRestoring = true;
         mIsCanceled = savedInstanceState.getBoolean(KEY_STATE_CANCELED, false);
+        mOverlayShown = savedInstanceState.getBoolean(KEY_STATE_OVERLAY_SHOWN, false);
         mPreviousRotation = savedInstanceState.getInt(KEY_STATE_PREVIOUS_ROTATION,
                 getDisplay().getRotation());
     }
@@ -404,6 +408,9 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
         updateTitleAndDescription(true);
         if (mRestoring) {
             startIconAnimation();
+        }
+        if (mOverlayShown) {
+            onUdfpsOverlayShown();
         }
     }
 
@@ -437,6 +444,7 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
         // showErrorDialog() will cause onWindowFocusChanged(false), set mIsCanceled to false
         // before showErrorDialog() to prevent that another error dialog is triggered again.
         mIsCanceled = true;
+        mOverlayShown = false;
         FingerprintErrorDialog.showErrorDialog(this, errorMsgId,
                 this instanceof SetupFingerprintEnrollEnrolling);
         cancelEnrollment();
@@ -450,6 +458,7 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
     @Override
     protected void onStop() {
         if (!isChangingConfigurations()) {
+            mOverlayShown = false;
             if (!WizardManagerHelper.isAnySetupWizard(getIntent())
                     && !BiometricUtils.isAnyMultiBiometricFlow(this)
                     && !mFromSettingsSummary) {
@@ -892,6 +901,7 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
     public void onUdfpsOverlayShown() {
         if (mCanAssumeUdfps) {
             findViewById(R.id.udfps_animation_view).setVisibility(View.VISIBLE);
+            mOverlayShown = true;
         }
     }
 
@@ -1066,6 +1076,7 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
 
                 @Override
                 public void onAnimationEnd(Animator animation) {
+                    mOverlayShown = false;
                     stopIconAnimation();
 
                     if (mProgressBar.getProgress() >= PROGRESS_BAR_MAX) {
