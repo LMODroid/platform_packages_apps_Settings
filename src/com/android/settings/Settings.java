7976de7cad4326a1a227c91d39172e0a32e5d920
Dhina17 <dhinalogu@gmail.com>
2025-03-03 17:33:30 +0530

    Settings: Setup activity to host LMO fragments
    Change-Id: I8708abd8d064522d0f93429d945d2a408f46a0fd

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -516,4 +516,6 @@ public class Settings extends SettingsActivity {
 
     public static class HearingDevicesActivity extends SettingsActivity { /* empty */ }
     public static class HearingDevicesPairingActivity extends SettingsActivity { /* empty */ }
+
+    public static class LMOSettingsActivity extends SettingsActivity { /* empty */}
 }
